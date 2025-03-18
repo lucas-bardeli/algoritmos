911270cdbc199f6c945b14c89da06f47985a2bcd
Lucas Bardeli de Bastiani Silva <lucas.silva1111@fatec.sp.gov.br>
2025-03-18 19:10:38 -0300

    Ajustes

--- Python/While-Ex1.py
+++
@@ -1,9 +0,0 @@
-
-num = 0 
-
-while (num != 1):
-    print()
-    print(f"O valor da variável num é: {num}")
-    num = int(input("Informe 1 para parar, ou qualquer número para continuar: "))
-
-print()
