ea1a57c5284b6c3156cbed6a252168202f696b2b
Lucas Bardeli de Bastiani Silva <lucas.silva1111@fatec.sp.gov.br>
2025-03-18 19:12:24 -0300

    Ajustes

---
+++ python/matriz-ex1.py
@@ -0,0 +1,13 @@
+
+linha = coluna = 0
+matriz = [[0]*4, [0]*4, [0]*4]
+
+print()
+for linha in range (0,3,1):
+    for coluna in range (0,4,1):
+        matriz[linha][coluna] = int(input(f"Informe o número para a posição {linha+1}, {coluna+1}: "))
+
+for linha in range (0,3,1):
+    print()
+    for coluna in range (0,4,1):
+        print (f"[{matriz[linha][coluna]}]", end = ' ')
